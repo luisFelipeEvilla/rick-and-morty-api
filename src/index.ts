86d92d03aed9546949b23c8c34fd8feeb8eeaf62
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-23 19:57:51 -0500

    add playground and voyager

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,18 @@ import { PORT } from './config';
 import { CharacterQueryType } from './queries/CharacterQueryType';
 import { GraphQLSchema } from 'graphql';
 import { graphqlHTTP } from 'express-graphql';
+import expressPlayground from 'graphql-playground-middleware-express';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware'
 
 const app = express();
 
 const schema = new GraphQLSchema({ query: CharacterQueryType });
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/graphql`);
