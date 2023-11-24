404dd3efe218862f9602ca42366456e765201f4b
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-23 19:39:31 -0500

    Add GraphQL endpoint and CharacterQueryType

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import 'dotenv/config.js';
 import express from 'express';
 import { PORT } from './config';
+import { CharacterQueryType } from './queries/CharacterQueryType';
+import { GraphQLSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
+
+const schema = new GraphQLSchema({ query: CharacterQueryType });
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/graphql`);
