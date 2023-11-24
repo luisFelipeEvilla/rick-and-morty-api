e5bfe3b93c33bd06bb909c48b384af5f4e70a97a
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-23 19:07:23 -0500

    initial setup with ts, dotenv, gitignore and express

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config.js';
+import express from 'express';
+import { PORT } from './config';
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/graphql`);
+});
